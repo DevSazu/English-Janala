a1fa853c8978d1e8e1a211ba88c2c3ff69ff198d
DevSazu <shazahansazu@gmail.com>
2026-08-15 22:59:36 +0600

    simplify html

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,29 +37,33 @@ loadButton();
  const displaylevel=(words)=>{
    const cardId=document.getElementById('card_id');
    cardId.innerHTML="";
+   
+   if(words.length===0){
+     cardId.innerHTML=`  <div class="text-white justify-center items-center col-span-full">
+                <p class="english_font text-4xl text-white justify-center items-center"> Lesson is not ready</p>
+                 
+            </div>`
+            return;
+   }
    words.forEach(word=>{
 
-    console.log(word)
+    
      const cardDiv=document.createElement('div');
-     cardDiv.className="bg-white w-[380px] p-[35px] rounded-2xl shadow-sm border border-gray-100 text-center";
+     cardDiv.className="bg-white w-[380px] p-[35px] rounded-2xl shadow-sm border border-gray-50 text-center";
      cardDiv.innerHTML=
-     `<h1 class="text-3xl font-extrabold text-black mb-2">  ${word.word}  </h1>
+     `<h1 class="text-3xl font-extrabold text-black mb-2">  ${word.word?word.word :'ওয়ার্ড পাওয়া যায়নি'}  </h1> 
             <p class="text-base font-semibold text-gray-800 mb-6"> Meaning /Pronunciation </p>
-            <p class="text-2xl font-bold text-gray-700 mb-10">  "${word.meaning} / ${word.pronunciation}" </p> 
+            <p class="text-2xl font-bold text-gray-700 mb-10">  "${word.meaning?word.meaning:"অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation :"উচ্চারন পাওয়া যায় নাই" }" </p> 
 
           <div class="flex justify-between items-center">
             <!-- Info Button -->
                 <button class="w-11 h-11 bg-blue-50 hover:bg-blue-100 rounded-xl flex items-center justify-center transition-colors">
-                    <svg class="w-5 h-5 fill-slate-700" viewBox="0 0 24 24">
-                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
-                    </svg>
+                    <i class="fa-solid fa-circle-info"></i>
                 </button>
 
                 <!-- Audio Button -->
                 <button class="w-11 h-11 bg-blue-50 hover:bg-blue-100 rounded-xl flex items-center justify-center transition-colors">
-                    <svg class="w-5 h-5 fill-slate-700" viewBox="0 0 24 24">
-                        <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/>
-                    </svg>
+                    <i class="fa-solid fa-volume"></i>
                 </button>
 
            </div>
